08af48f25699d75a4cc53f595324c9d84ac1ea4c
Rahul Kumar Mishra <mishrarahul32299@gmail.com>
2026-08-16 20:54:56 +0530

    thried

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ const httpServer = createServer(app);
 
 const io = new Server(httpServer, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://collabrative-white-bord.vercel.app",
   },
 });
 
